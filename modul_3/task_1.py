6e9e07409f4724a4b8bd7d80e8b7056138c11db8
nicklaswp05 <nicklaswp05@gmail.com>
2026-09-25 14:21:18 +0200

    Øvingsoppgave 1

--- modul_3/task_1.py
+++ modul_3/task_1.py
@@ -1,1 +1,11 @@
+navn = ("Nicklas")
+alder = (21)
+høyde = (179.9)
+er_student = (True)
+hobbyer = ["spill", "vekt løfting", "TV/film", "musikk"]
 
+print ("Navn", (type(navn)))
+print ("Jeg er", (type(alder)), "år gammel")
+print ("Jeg er ikke spesielt høyd på en høyde av", (type(høyde)), "cm.")
+print ("Student?", (type(er_student)))
+print ("Hobbyene mine består av:", (type(hobbyer)))
